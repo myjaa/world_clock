ec5cf03ccac0819a99ac7f9f9211f2f5ebf5f315
Mohammad Yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-08-23 07:57:47 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:worldclock/pages/home.dart';
+import 'package:worldclock/pages/choose_location.dart';
+import 'package:worldclock/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  theme: ThemeData.dark(),
+  routes: {
+    '/':(context) => Loading(),
+    '/home':(context) =>Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+  )
+  );
+
